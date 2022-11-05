e00a8c49bc561b14f9ba82aaef4cd40d5493a6d6
insinnox <marcel.reiter2077@gmail.com>
2022-11-05 18:47:15 +0100

    style changes in about

--- components/About.jsx
+++ components/About.jsx
@@ -9,9 +9,9 @@ function About() {
   return (
     <div
       id='about'
-      className='w-full md:h-screen p-2 flex items-center overflow-x-hidden px-14'
+      className='w-full md:h-screen p-2 flex items-center overflow-x-hidden '
     >
-      <section className='secHidden2 max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+      <section className='secHidden2 max-w-[1240px] m-auto md:grid grid-cols-3 gap-8 px-14'>
         <div className='col-span-2'>
           <p className='uppercase text-xl tracking-widest text-[#e92c2c]'>
             About
